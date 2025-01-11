43061a3a3abfb37ae5d066ce37bfccb727dc3c84
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2025-01-11 12:17:23 +0000

    added toasts to remove-from-bag view

--- bag/views.py
+++ bag/views.py
@@ -79,6 +79,7 @@ def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
@@ -88,11 +89,15 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')    
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
+    
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)

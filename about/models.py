5bb34db03ea656cf10d00223f7e5ae28d6f2fc9a
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2025-01-15 03:41:01 +0000

    created about models

--- about/models.py
+++ about/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+# Create About model
+class About(models.Model):
+    """
+    Stores single about us request.
+    """
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"

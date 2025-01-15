98eff730934c9430f022c0ad07ff430fbe865976
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2025-01-15 03:49:50 +0000

    created about us views and urls

--- about/views.py
+++ about/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import About
+from .forms import AboutForm
 
 # Create your views here.
+
+
+# Created about_me view that renders all info of about model
+def about_us(request):
+    """
+    Display and insert record in About model.
+    """
+    aboutset = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about_us.html",
+        {
+            "about": aboutset
+        },
+    )

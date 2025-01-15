25817c3414852e03157fb14e34fc8bb1b5907aab
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2025-01-15 10:50:22 +0000

    bug fixes

--- curry_tales/settings.py
+++ curry_tales/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-mouneeshwari-currytales-tx7q1wfc73h.ws.codeinstitute-ide.net',

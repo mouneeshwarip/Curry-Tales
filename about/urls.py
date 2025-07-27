6d5a6a8cd7a7eaaf61af4924eddf7a5f1075ed89
Balakumaran Moorthi <balakumaranmoorthi@BalakumaransAir.fritz.box>
2025-07-28 00:48:53 +0200

    modified about_us url

--- about/urls.py
+++ about/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about_us, name='about'),
+    path('', views.about_us, name='about_us'),
     path('edit/', views.edit_about_us, name='edit_about_us'),
     path('contact/', views.contact, name='contact'),
 ]
